4403fb22f25d668a66f476103f92e9bbd4ea0a1a
PurplePenguin4102 <joseph.h.ray@gmail.com>
2017-04-01 18:28:37 +1000

    Unit tests now functioning for Stellar Object, octants 0-7 displayed correctly

--- StarCommander/StellarObject.rb
+++ StarCommander/StellarObject.rb
@@ -12,31 +12,37 @@ class StellarObject
 		dx = @x - stellar_object.x
 		dy = @y - stellar_object.y
 		dz = @z - stellar_object.z
-		Math.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
+		Math.sqrt(dx ** 2 + dy ** 2 + dz ** 2).round(2)
 	end
 
 	def display_distance(stellar_object)
 		"#{stellar_object.name} is #{self.distance_to(stellar_object)}Mkm away"
 	end
 
-	#phi displays the angle of difference in yz plane
+	#phi displays the angle of lift from xy plane from the origin with z=0 
 	def get_phi(stellar_object)
 		dy = stellar_object.y - @y
 		dz = stellar_object.z - @z
 		yz_hyp = Math.sqrt(dy ** 2 + dz ** 2)
-		Math.asin(dz / yz_hyp)
+		rad =  Math.asin(dz / yz_hyp)
+		angle = Angle.new rad * 180 / Math::PI
+		return angle
 	end
 
-	#theta displays the angle of difference in the xy plane
+	#theta displays the angle of difference in the xy plane from the y+ x=0 axis
 	def get_theta(stellar_object)
 		dx = stellar_object.x - @x
 		dy = stellar_object.y - @y
 		xy_hyp = Math.sqrt(dx ** 2 + dy ** 2)
-		Math.asin(dy / xy_hyp)
+		rad = Math.asin(dx / xy_hyp)
+		angle = Angle.new rad * 180 / Math::PI
+		angle.deg = 180 - angle.deg if dy < 0
+		angle.deg = angle.deg - 360 if angle.deg > 180
+		return angle
 	end
 
 	def display_coords_rads(stellar_object)
-		%Q(Relative to universal coordinated center #{stellar_object.name} is 
+		%Q(Relative to universal coordinated center pointing y+ #{stellar_object.name} is 
 #{self.distance_to(stellar_object)}Mkm away at 
 \u03B8 #{self.get_theta(stellar_object)} and 
 \u03D5 #{self.get_phi(stellar_object)}).encode('utf-8')
--- StarCommander/Tests/StellarObjectTests.rb
+++ StarCommander/Tests/StellarObjectTests.rb
@@ -35,7 +35,7 @@ class StellarObjectTests < Minitest::Test
 	def test_distance_to_object
 		origin = StellarObject.new [0, 0, 0]
 		obj = StellarObject.new [2, 3, 4]
-		testDistance = Math.sqrt(2 ** 2 + 3 ** 2 + 4 ** 2)
+		testDistance = Math.sqrt(2 ** 2 + 3 ** 2 + 4 ** 2).round(2)
 
 		res = origin.distance_to(obj)
 		assert res == testDistance
@@ -45,28 +45,70 @@ class StellarObjectTests < Minitest::Test
 		origin = StellarObject.new [0, 0, 0]
 		obj = StellarObject.new [2, 3, 4]
 		friendly = origin.display_distance(obj)
-		
-		assert "object is 5Mkm away" == friendly
+
+		assert "object is 5.39Mkm away" == friendly
 
 	end
 
 	def test_get_theta
 		origin = StellarObject.new [0, 0, 0]
-		obj = StellarObject.new [3, 4, 5]
-		theta = Math.asin(4 / 5.0)
+		obj = StellarObject.new [1, 1, 5]
+		theta_rad = Math.asin(1 / Math.sqrt(2))
+		theta_deg = Angle.new (theta_rad * 180 / Math::PI)
 		real_theta = origin.get_theta(obj)
-		puts "==="
-		puts real_theta, theta
-		puts "==="
-		assert theta == real_theta
+
+		assert theta_deg.deg == real_theta.deg && theta_deg.min == real_theta.min && theta_deg.sec == real_theta.sec
 	end
 
 	def test_get_relative_coordinates
 		origin = StellarObject.new [0, 0, 0]
-		obj = StellarObject.new [3, 4, 5]
+		obj = StellarObject.new [1, 1, 1]
 		puts "==="
-		puts origin.display_coords_rads(obj)
+		puts "Octant 0", origin.display_coords_rads(obj)
+		puts "==="
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [1, -1, 1]
+		puts "==="
+		puts "Octant 1", origin.display_coords_rads(obj)
+		puts "==="
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [-1, -1, 1]
+		puts "==="
+		puts "Octant 2", origin.display_coords_rads(obj)
+		puts "==="
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [-1, 1, 1]
+		puts "==="
+		puts "Octant 3", origin.display_coords_rads(obj)
+		puts "==="
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [1, 1, -1]
+		puts "==="
+		puts "Octant 4", origin.display_coords_rads(obj)
+		puts "==="
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [1, -1, -1]
+		puts "==="
+		puts "Octant 5", origin.display_coords_rads(obj)
+		puts "==="
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [-1, -1, -1]
+		puts "==="
+		puts "Octant 6", origin.display_coords_rads(obj)
+		puts "==="
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [-1, 1, -1]
+		puts "==="
+		puts "Octant 7", origin.display_coords_rads(obj)
 		puts "==="
 	end
 
+	def test_math_works
+		origin = StellarObject.new [0, 0, 0]
+		obj = StellarObject.new [3, 4, 5]		
+		origin1 = StellarObject.new [1, 1, 1]
+		obj1 = StellarObject.new [4, 5, 6]
+
+		assert origin.display_coords_rads(obj) == origin1.display_coords_rads(obj1)
+	end
 end
